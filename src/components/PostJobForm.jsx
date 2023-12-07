2b3de64c6fedbf8ea71dfee8b3110fb865cc90cc
Huseyin Hepyalniz <huseyinhepyalniz@huseyins-air.speedport.ip>
2023-12-07 15:48:36 +0100

    PostJobForm component created

---
+++ src/components/PostJobForm.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+
+const initialFormState = {
+  jobTitle: '',
+  company: '',
+  jobLocation: '',
+  jobType: '',
+  description: '',
+};
+
+
+export default PostJobForm;
