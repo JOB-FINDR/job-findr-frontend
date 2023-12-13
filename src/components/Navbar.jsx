b3eda85300053684925adae6f87a1ce36e3174f4
NISHA PATIL <prashantpatil@prashants-air.kpn>
2023-12-13 18:08:04 +0100

    changed the location of logout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
 import { Link } from "react-router-dom";
 import logo from "../images/logo1.png";
-import {useContext } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
-
 function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
-
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
@@ -24,28 +22,12 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-            <h1>Job Findr</h1>
-          </Link>
-
-          <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link active" to="/">
                 Home
               </Link>
             </li>
-            {isLoggedIn && (
-              <>
-              <li className="nav-item">
-                <Link className="nav-link" to="/jobs">
-                  Jobs
-                </Link>
-                
-              </li>
-              
-              </>
-              
-            )}
             {!isLoggedIn && (
               <>
                 <li className="nav-item">
@@ -53,34 +35,47 @@ function Navbar() {
                     Login
                   </Link>
                 </li>
-                <li className="nav-item ">
+              </>
+            )}
+            {isLoggedIn && (
+              <li className="nav-item">
+                <Link className="nav-link" to="/jobs">
+                  Jobs
+                </Link>
+              </li>
+            )}
+            <li className="nav-item">
+              <Link className="nav-link" to="/create">
+                Post a Job
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            {isLoggedIn && (
+              <>
+                <li className="nav-item">
+                  <span className="nav-link">Hi, {user.name}</span>
+                </li>
+                <li className="nav-item ms-2">
+                  <button
+                    className="btn btn-link nav-link"
+                    onClick={logOutUser}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            )}
+            {!isLoggedIn && (
+              <>
+                <li className="nav-item">
                   <Link className="nav-link" to="/signup">
                     SignUp
                   </Link>
                 </li>
               </>
             )}
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/create">
-                Post a Job
-              </Link>
-            </li>
-
-            {isLoggedIn && (
-              <>
-              <li className="nav-item">
-                  <span className="nav-link">Hi, {user.name}</span>
-                </li>
-                <li className="nav-item ms-2">
-                  <button className="btn btn-link nav-link" onClick={logOutUser}>
-                    Logout
-                  </button>
-                </li>
-              </>
-            )}
           </ul>
-        
         </div>
       </div>
     </nav>
