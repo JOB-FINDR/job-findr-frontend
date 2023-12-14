0d3b533c034aa597e6b4189de3500109da6cd62f
NISHA PATIL <prashantpatil@prashants-air.kpn>
2023-12-14 16:39:02 +0100

    fixed error displaying job details

--- src/pages/JobListPage.jsx
+++ src/pages/JobListPage.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import { useNavigate } from "react-router-dom";
 
 function JobListPage(props) {
   const [jobs, setJobs] = useState([]);
-
+  const navigate = useNavigate();
   const getAllJobs = () => {
-
     // Get the token from the localStorage
     const storedToken = localStorage.getItem("authToken");
     // Send the token through the request "Authorization" Headers
@@ -19,17 +18,17 @@ function JobListPage(props) {
       .then((response) => setJobs(response.data))
       .catch((error) => console.log(error));
   };
+
   // We set this effect will run only once, after the initial render
   // by setting the empty dependency array - []
   useEffect(() => {
     getAllJobs();
   }, []);
 
-  
-
   const handleDelete = (jobId) => {
     axios
       .delete(`${import.meta.env.VITE_API_URL}/api/jobs/${jobId}`)
+
       .then(() => {
         props.getJobs();
         console.log("Job deleted");
@@ -38,6 +37,7 @@ function JobListPage(props) {
         console.error("Error deleting job:", error);
       });
     console.log(`Delete the product with id ${jobId}`);
+    //navigate("/jobs");
   };
 
   return (
@@ -45,11 +45,8 @@ function JobListPage(props) {
       <div className="row">
         {props.jobs.map((job) => (
           <div className="col-md-6 mb-4" key={job._id}>
-            <Link
-              to={`/job/${job._id}`}
-              style={{ textDecoration: "none" }}
-            >
-              <div className="card border-secondary border-3">
+            <div className="card border-secondary border-3">
+              <Link to={`/job/${job._id}`} style={{ textDecoration: "none", color: "black" }}>
                 <div className="card-body">
                   <h5 className="card-title">{job.title}</h5>
                   <p className="card-text">{job.company}</p>
@@ -57,23 +54,23 @@ function JobListPage(props) {
                   <p className="card-text">Type: {job.jobType}</p>
                   <p className="card-text">Description: {job.description}</p>
                   <p className="card-text">Salary: {job.salary}</p>
-                  <div className="btn-group mx-auto mt-3">
-                    <button
-                      className="btn btn-danger"
-                      onClick={() => handleDelete(job._id)}
-                    >
-                      Delete
-                    </button>
-                    <Link to={`/update/${job._id}`}>
-                      <button className="btn btn-primary ml-2">Edit</button>
-                    </Link>
-                    <Link to={`/application/${job._id}`}>
-                  <button className="btn btn-success ml-2">Apply</button>
-                  </Link>
-                  </div>
                 </div>
+              </Link>
+              <div className="btn-group mx-auto mt-3">
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(job._id)}
+                >
+                  Delete
+                </button>
+                <Link to={`/update/${job._id}`}>
+                  <button className="btn btn-primary ml-2">Edit</button>
+                </Link>
+                <Link to={`/application/${job._id}`}>
+                  <button className="btn btn-success ml-2">Apply</button>
+                </Link>
               </div>
-            </Link>
+            </div>
           </div>
         ))}
       </div>
